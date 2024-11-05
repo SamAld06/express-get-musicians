6a8d78160ef68c8b355d3071b1930499b5806ae7
SamAld06 <samgaldred@gmail.com>
2024-11-05 15:56:46 +0000

    completed parts 1-3

--- index.test.js
+++ index.test.js
@@ -12,11 +12,39 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test("can respond with the full array of musician objects", async () =>{
+        const response = await request(app).get("/musicians")
+        expect(response.statusCode).toBe(200)
+        const responseData = JSON.parse(response.test)
+        expect(responseData).toEqual(expect.arrayContaining(responseData.map(musician => expect.objectContaining({
+            name: expect.any(String),
+            instrument: expect.any(String)
+        }))))
+    })
     
-    
+    test("can respond with the correct musician", async () =>{
+        const response = await request(app).get("/musicians/1")
+        const responseData = JSON.parse(response.text)
+        expect(response.statusCode).toBe(200)
+        expect(responseData.id).toBe(1)
+    })
 
+    test("can add to the list of musicians", async () => {
+        const response = await request(app).post("/musicans")
+        const responseData = JSON.parse(response.text)
+        expect(response.statusCode).toBe(200)
+    })
 
+    test("can delete a musician from the full list", async () => {
+        const response = await request(app).delete("/musicians/1")
+        expect(response.statusType).toBe(200)
+        const responseData = JSON.parse(response.text)
+    })
 
-
+    test("can update a musician in the list of musicians", async () => {
+        const response = await request(app).put("/musicians/1")
+        expect(response.statusType).toBe(200)
+        const responseData = JSON.parse(response.text)
+    })
     
 })
--- src/app.js
+++ src/app.js
@@ -2,12 +2,37 @@ const express = require("express");
 const app = express();
 const { Musician } = require("../models/index")
 const { db } = require("../db/connection")
-
-const port = 3000;
+app.use(express.json())
+app.use(express.urlencoded())
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (req, res) => {
+    const allMusicians = await Musician.findAll({})
+    res.json(allMusicians)
+})
 
+app.get("/musicians/id:", async (req, res) => {
+    const musician = await Musician[req.params.id - 1]
+    res.json(musician)
+})
+
+app.post("/musicians", async (req, res) => {
+    const musician = await Musician.create(req.body)
+    res.json(musician)
+})
+
+app.put("/musicians", (res, req) => {
+    let index = req.params.id
+    Musician[index] = req.body
+    res.json(index)
+})
+
+app.delete("/musicians/:id", (res, rep) => {
+    const index = req.params.id
+    Musician.splice(index, 1)
+    res.json(index)
+})
 
 
 
