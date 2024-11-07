d5084fcbd13e126ac17d53f5ace892ecd47ec0a1
SamAld06 <samgaldred@gmail.com>
2024-11-07 17:03:57 +0000

    completed all workshop task for today (exc extension)

--- index.test.js
+++ index.test.js
@@ -10,42 +10,97 @@ const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 const syncSeed = require("./seed.js")
 
+beforeAll(async () => {
+    await syncSeed();
+    const musicians = await Musician.findAll();
+    restQuantity = musicians.length;
+});
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    test("can respond with the full array of musician objects", async () =>{
-        const response = await request(app).get("/musicians")
-        expect(response.statusCode).toBe(200)
-        const responseData = JSON.parse(response.test)
-        expect(responseData).toEqual(expect.arrayContaining(responseData.map(musician => expect.objectContaining({
-            name: expect.any(String),
-            instrument: expect.any(String)
-        }))))
+describe("Testing GET /resturants,", () => {
+    it("responds with the array of restaurants", async () => {
+        const musicians = await request(app).get("/Musician")
+        expect(musicians.body).toEqual({
+            name: 'Mick Jagger',
+            instrument: 'Voice'
+          },
+          {
+            name: 'Drake',
+            instrument: 'Voice',
+          },
+          {
+            name: 'Jimi Hendrix',
+            instrument: 'Guitar'
+          })
     })
-    
-    test("can respond with the correct musician", async () =>{
-        const response = await request(app).get("/musicians/1")
-        const responseData = JSON.parse(response.text)
-        expect(response.statusCode).toBe(200)
-        expect(responseData.id).toBe(1)
+    it("responds with a status code of 200", async () => {
+        const status = await request(app).get("/Musician")
+        expect(status.statusCode).toBe(200)
+    })
+    it("returns the correct number of musicians", async () => {
+        const musicians = await request(app).get("/Musician")
+        expect(musicians.body.length).toEqual(restQuantity)
+    })
+    it("returns the correct musician data", async () => {
+        const musician= await request(app).get("/Musician")
+        expect(musician.body).toContainEqual(
+            expect.objectContaining({
+                id: 1,
+                name: 'Mick Jagger',
+                instrument: 'Voice',
+            })
+        )
+    })
+})
+
+it("GET /Musician/:id", async () => {
+    const musician = await request(app).get("/Musician/2")
+    expect(musician.body).toEqual(
+        expect.objectContaining({
+            id: 2,
+            name: 'Drake',
+            instrument: 'Voice',
+        })
+    )
+})
+
+describe("POST /Musician, ", () => {
+
+    it("request returns the updated with the new value", async () => {
+        const response = await request(app)
+        .post("/Musician")
+        .send({name: "Dave Grohl", instrument: "Guitar"})
+        expect(response.body.length).toEqual(restQuantity+1)
     })
 
-    test("can add to the list of musicians", async () => {
-        const response = await request(app).post("/musicans")
-        const responseData = JSON.parse(response.text)
-        expect(response.statusCode).toBe(200)
+    it("returns error when name is empty", async () => {
+        const status = await request(app)
+        .post("/Musician")
+        .send({instrument: "Guitar"})
+        expect(status.body).toHavePropety("error")
+        expect(Array.isArray(status.body.error)).toBe(true)
     })
 
-    test("can delete a musician from the full list", async () => {
-        const response = await request(app).delete("/musicians/1")
-        expect(response.statusType).toBe(200)
-        const responseData = JSON.parse(response.text)
+    it("returns error when instrument is empty", async () => {
+        const status = await request(app)
+        .post("/Musician")
+        .send({name: "Dave Grohl"})
+        expect(status.body).toHavePropety("error")
+        expect(Array.isArray(status.body.error)).toBe(true)
     })
+})
 
-    test("can update a musician in the list of musicians", async () => {
-        const response = await request(app).put("/musicians/1")
-        expect(response.statusType).toBe(200)
-        const responseData = JSON.parse(response.text)
-    })
-    
+it("PUT /Musician/:id", async () => {
+    await request(app)
+    .put("/Musician/1")
+    .send({name: "Dave Grohl", instrument: "Guitar"})
+    const musician = await Musician.findByPk(1)
+    expect(musician.name).toEqual("Dave Grohl")
+    expect(musician.instrument).toEqual("Guitar")
+})
+
+it("DELETE /Musician/:id", async () => {
+    await request(app).delete("/Musician/1")
+    const musician =await Musician.findAll()
+    expect(musician.length).toEqual(restQuantity)
+    expect(musician[0].id).not.toEqual(1)
 })
